9a7102ab4b2e5e963c7af4c5a7dfb119660fe10c
sarath777-glitch <sarath777@gmail.com>
2026-01-12 10:37:04 +0530

    Committing 1 item from workspace 3dad38d0-7f3f-486e-9809-46377c147d77

--- TestFolder/Notebook_1.Notebook/notebook-content.py
+++ TestFolder/Notebook_1.Notebook/notebook-content.py
@@ -45,3 +45,14 @@ display(df)
 # META   "language": "python",
 # META   "language_group": "synapse_pyspark"
 # META }
+
+# CELL ********************
+
+##Hi. This is for testing
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
