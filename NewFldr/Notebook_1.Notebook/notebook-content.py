49f91f7011cefc6e4470feb599d85696208a87d0
sarath777-glitch <sarath777@gmail.com>
2026-01-23 15:58:08 +0530

    Committing 1 item from workspace a4279a93-22d5-473f-b891-278045840021

--- NewFldr/Notebook_1.Notebook/notebook-content.py
+++ NewFldr/Notebook_1.Notebook/notebook-content.py
@@ -6,7 +6,18 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "a326b7f7-8671-4305-8900-b800c08e2823",
+# META       "default_lakehouse_name": "sampleLK",
+# META       "default_lakehouse_workspace_id": "a4279a93-22d5-473f-b891-278045840021",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "a326b7f7-8671-4305-8900-b800c08e2823"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # CELL ********************
@@ -14,6 +25,19 @@
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
 
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df = spark.read.format("csv").option("header","true").load("Files/sales.csv")
+# df now is a Spark DataFrame containing CSV data from "Files/sales.csv".
+display(df)
 
 # METADATA ********************
 
