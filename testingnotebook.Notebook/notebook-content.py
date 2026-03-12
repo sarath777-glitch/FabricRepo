639a0a27147fd73a7a7ba1839f45df1e34c28de3
sarath777-glitch <sarath777@gmail.com>
2026-03-12 22:37:22 +0530

    Committing 1 item from workspace bed7ad4a-4141-4baa-8b81-39808a69fe08

---
+++ testingnotebook.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "801c218e-7145-4b1e-8f50-544ebc890c40",
+# META       "default_lakehouse_name": "TestingLakehouse",
+# META       "default_lakehouse_workspace_id": "bed7ad4a-4141-4baa-8b81-39808a69fe08",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "801c218e-7145-4b1e-8f50-544ebc890c40"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
